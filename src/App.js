bc755393fd519392514b8b2901d37a2349e8d5cd
guimariz <guimariz@gmail.com>
2022-01-06 09:39:41 -0300

    Integrando Routes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
-import { Route } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import NavBar from './components/navbar';
 import Products from './components/products';
 import Posts from './components/posts';
 import Home from './components/home';
-import Dashboard from './components/admin/dashboard';
+import Dashboard from './components/Admin/dashboard.jsx';
 import ProductDetails from './components/productDetails';
 import NotFound from './components/notFound';
 
 function App() {
   return (
-    <div>
+    <>
       <NavBar />
       <div className="content">
-        <Route path="/products" component={Products} />
-        <Route path="/posts" component={Posts} />
-        <Route path="/dashboard" component={Dashboard} />
-        <Route path="/admin" component={Admin} />
-        <Route path="/" component={Home} />
+        <Switch>
+          <Route path="/products/:id" component={ProductDetails} />
+          <Route
+            path="/products"
+            render={(props) => <Products sortBy="newest" {...props} />}
+          />
+          <Route path="/posts/:year?/:month?" component={Posts} />
+          <Route path="/admin/dashboard" component={Dashboard} />
+          <Redirect from="/messages" to="/posts" />
+          <Route path="/not-found" component={NotFound} />
+          <Route path="/" exact component={Home} />
+          <Redirect to="/not-found" />
+        </Switch>
       </div>
-    </div>
+    </>
   );
 }
 
---
+++ src/components/posts.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import queryString from 'query-string';
+
+const Posts = ({ match, location }) => {
+  const { sortBy } = queryString.parse(location.search)
+  
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      Year: { match.params.year } , Month: { match.params.month }
+    </div>
+  )
+}
+
+export default Posts; 
